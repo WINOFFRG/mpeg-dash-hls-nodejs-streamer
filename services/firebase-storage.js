2f799a9c8a1c731ed5a79f213999172abc857b81
winoffrg <rohangupta1528@gmail.com>
2022-01-21 01:23:42 +0530

    Completed Major

--- services/firebase-storage.js
+++ services/firebase-storage.js
@@ -2,6 +2,8 @@ const firebase = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
 const firebaseConfig = require("../firebase.js");
 const { storage } = require("firebase-admin");
+const path = require("path");
+const fsPromises = require("fs").promises;
 
 firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount),
@@ -9,5 +11,69 @@ firebase.initializeApp({
 });
 
 const bucketStorage = firebase.storage().bucket();
+const bucketBase = 'https://storage.googleapis.com/nodejs-streaming.appspot.com';
 
-module.exports = bucketStorage;
+async function uploadRecursively(directory, destinationBase) {
+
+    try {
+        const files = await fsPromises.readdir(directory);
+        
+        for(const file of files){
+            const currPath = `${directory}\\${file}`;
+            const stat = await fsPromises.lstat(currPath);
+            
+            if(stat.isDirectory()){
+                await uploadRecursively(currPath, `${destinationBase}/${file}`);
+            }
+            else {
+                await bucketStorage.upload(currPath, { destination: `${destinationBase}/${file}` });
+            }
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function upload(sessionObj) {
+    
+    const uploadFolderPath = path.join(__dirname, `../uploads/${sessionObj.session}/baked`);
+    const destinationBase = `uploads/${sessionObj.session}/${sessionObj.contentId}`;
+
+    try {
+        await uploadRecursively(uploadFolderPath, destinationBase);
+        const manifestUrl = `${bucketBase}/${destinationBase}/manifest.mpd`;
+        return manifestUrl;
+
+    } catch (error) {
+        console.log(error["errors"]);
+        return error;
+    }
+}
+
+/* Set CORS Config on Bucket*/
+async function setBucketCors() {
+    
+    const corsConfiguration = [{
+        "origin": ["*"],
+        "method": ["GET"],
+        "maxAgeSeconds": 3600
+      }]; 
+    bucketStorage.setCorsConfiguration(corsConfiguration).then(function(data) {
+        const apiResponse = data[0];
+        console.log(apiResponse.cors);
+    });
+}
+
+/* Remove CORS Config from Bucket*/
+async function removeBucketCors() {
+    await bucketStorage.setCorsConfiguration([]);
+  
+    console.log(`Removed CORS configuration from bucket`);
+} 
+
+let firebaseService = {
+    upload,
+};
+
+module.exports = firebaseService;
--- services/video-service.js
+++ services/video-service.js
@@ -1,10 +1,14 @@
 const multer = require('multer');
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsPromise = require('fs').promises;
 const path = require('path');
 const logger = require('../loaders/logger');
 const session = require('../api/controller/session');
 const ffmpeg = require('./fluent-ffmpeg');
 const bento4 = require('./bento4-service');
+const firebaseService = require('./firebase-storage');
+const glob = require('glob');
+const rimraf = require('rimraf');
 
 const storage = multer.diskStorage({
     destination: async (req, file, cb) => {
@@ -15,32 +19,36 @@ const storage = multer.diskStorage({
             console.log(file);
         */
 
-        /* Multer by default doesn't makes file if doesn't exisst, so use FS*/
-        await fs.mkdir(filePath, {recursive: true});
+        /* Multer by default doesn't makes file if doesn't exisst, so use FSPromise*/
+        await fsPromise.mkdir(filePath, {
+            recursive: true
+        });
         cb(null, filePath);
     },
-    
+
     filename: (req, file, cb) => {
         cb(null, `${ req.locals.contentId + path.extname(file.originalname).toLowerCase() }`);
     },
 });
 
-const multerUpload = multer({ 
+const multerUpload = multer({
     storage: storage,
-    limits: { fileSize: 100 * 1024 * 1024 },
+    limits: {
+        fileSize: 100 * 1024 * 1024
+    },
     fileFilter: (req, file, cb) => {
 
         req.locals.fileType = path.extname(file.originalname).toLowerCase();
 
         const allowedTypes = ['.mp4', '.webm', '.mkv', '.avi', '.mpeg'];
 
-        if(!allowedTypes.includes(path.extname(file.originalname).toLowerCase())) {    
+        if (!allowedTypes.includes(path.extname(file.originalname).toLowerCase())) {
             return cb(
                 new multer.MulterError('CUSTOM', {
                     message: 'File type not allowed',
                     code: 'FILE_TYPE_NOT_ALLOWED',
                 })
-            );   
+            );
         }
 
         cb(null, true);
@@ -50,18 +58,18 @@ const multerUpload = multer({
 /*
     .single('file') defines the name of the file input field
 */
-    
+
 const multerPromise = (req, res) => {
 
     return new Promise((resolve, reject) => {
 
         multerUpload(req, res, function (err) {
 
-        if(err) {
-            reject(err);
-        }
+            if (err) {
+                reject(err);
+            }
 
-        resolve(req.file);
+            resolve(req.file);
         });
     });
 };
@@ -69,7 +77,7 @@ const multerPromise = (req, res) => {
 class videoManager {
 
     static async uploadVideo(req, res) {
-    
+
         try {
             await multerPromise(req, res);
 
@@ -81,20 +89,20 @@ class videoManager {
             };
 
         } catch (error) {
-            
-            if(error instanceof multer.MulterError) {
-                
-                if(!error.message) error.message = error.field.message;
-                
+
+            if (error instanceof multer.MulterError) {
+
+                if (!error.message) error.message = error.field.message;
+
                 return {
                     status: 400,
                     data: {
                         message: "Request failed to process.",
                         error: error.message || error.field.message,
-                    }, 
+                    },
                 }
             };
-            
+
             await videoManager.preCleanUp(req);
             return error;
         }
@@ -110,14 +118,14 @@ class videoManager {
             currentSession = await session.getStaus(sessionObj.session);
             currentSession = currentSession['data'];
             currentSession['data'] = {
-                'type' : 'timeline',
+                'type': 'timeline',
                 'status': 'processing',
                 'jobs': {
                     health: '',
                     fragments: '',
                     converting: '',
-                    cleaning: '',
                     uploading: '',
+                    cleaning: '',
                 }
             }
 
@@ -132,42 +140,51 @@ class videoManager {
             logger.info("🔰 2/5 | Checking Video");
             const fragmentationRequired = await bento4.checkFragments(sessionObj);
 
-            logger.info("🔰 3/5 | Fragmenting Video");
-            if(fragmentationRequired) {
+            logger.info("🔰 2/5 | Fragmenting Video");
+            if (fragmentationRequired) {
                 const response = await bento4.fragmentVideo(sessionObj);
                 jobs['fragments'] = 'success';
 
-                if(response instanceof Error) {
+                if (response instanceof Error) {
                     jobs['fragments'] = 'failed';
                     await session.updateSession(currentSession);
                     throw res;
                 }
-            }
-            else {
+            } else {
                 jobs['fragments'] = 'skipped';
 
                 const fileExt = sessionObj.fileType;
                 const oldFileName = path.join(__dirname, `../uploads/${sessionObj.session}/${sessionObj.contentId}${fileExt}`);
                 const newFileName = path.join(__dirname, `../uploads/${sessionObj.session}/${sessionObj.contentId}_fragmented${fileExt}`);
-                await fs.rename(oldFileName, newFileName);
+                await fsPromise.rename(oldFileName, newFileName);
             }
             await session.updateSession(currentSession);
 
-            logger.info("🔰 4/5 | Processing Video");
+            logger.info("🔰 3/5 | Processing Video");
             await bento4.processVideo(sessionObj);
             jobs['converting'] = 'success';
             await session.updateSession(currentSession);
 
-            // await videoManager.postCleanUp(sessionObj);
+            logger.info("🔰 4/5 | Uploading Video");
+            const url = await firebaseService.upload(sessionObj);
+            jobs['uploading'] = 'success';
+            currentSession['publicURL'] = url;
+            await session.updateSession(currentSession);
 
-            //uploadVideo()
+            logger.info("🔰 5/5 | Cleaning Up");
+            const dirPath = path.join(__dirname, `../uploads/${sessionObj.session}`);
+            await videoManager.postCleanUp(dirPath);
+            jobs['cleaning'] = 'success';
+            currentSession['data']['status'] = 'complete';
+            await session.updateSession(currentSession);
+
             //scheduleJobs()
         } catch (error) {
 
             console.log("Fallback in Catch");
             logger.error(error);
 
-            if(error.name === 'functional') {
+            if (error.name === 'functional') {
                 currentSession["data"]["status"] = "failed";
                 await session.updateSession(currentSession);
             }
@@ -180,26 +197,41 @@ class videoManager {
     static async preCleanUp(sessionObj) {
 
         try {
-            const filePath = path.join(__dirname, `../uploads/${ sessionObj.session }`);
-            
-            await fs.rm(filePath, {recursive: true});
+            const filePath = path.join(__dirname, `../uploads/${sessionObj.session}`);
+
+            await fsPromise.rm(filePath, {
+                recursive: true
+            });
 
         } catch (error) {
             console.log(error);
         }
+
+        return;
     }
 
-    static async postCleanUp(sessionObj) {
+    static async postCleanUp(path) {
 
-        const filePath = path.join(__dirname, `../uploads/${ sessionObj.session }`);
-        
-        const files = await fs.readdir(filePath);
-        
-        for(const file of files){
-            console.log(file);
+        const files = await fsPromise.readdir(path);
+
+        try {
+            for(const file of files) {     
+                
+                if(file == 'status.json') continue;
+
+                const currPath = `${path}\\${file}`;
+   
+                await fsPromise.rm(currPath, {
+                    recursive: true
+                });
+            }            
+
+        } catch (error) {
+            logger.error(error);
+            return error;
         }
     }
-    
+
 }
 
 module.exports = {
